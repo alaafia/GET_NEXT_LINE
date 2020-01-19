c1021a35cc0128494eed05015c1a09e94bc45093
aicha laafia <54075891+alaafia@users.noreply.github.com>
2020-01-20 00:28:31 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,109 +12,96 @@
 
 #include "get_next_line.h"
 
-char			*ft_substr(char const *s, unsigned int start, size_t len)
+size_t	ft_strlen(const char *str)
 {
 	size_t	i;
-	size_t	ft_len;
-	char	*str;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	char	*t;
+
+	i = 0;
+	while (s1[i])
+		i++;
+	if (!(t = malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		t[i] = s1[i];
+		i++;
+	}
+	t[i] = '\0';
+	return (t);
+}
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*fresh;
+	int		i;
 
 	if (!s)
 		return (NULL);
-	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
+	fresh = malloc((len + 1) * sizeof(char));
+	i = 0;
+	if (!fresh)
 		return (NULL);
-	i = 0;
-	ft_len = ft_strlen(s);
-	while (i < len && start < ft_len)
-		str[i++] = s[start++];
-	str[i] = '\0';
-	return (str);
-}
-
-char			*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*res;
-	int		i;
-	int		k;
-
-	if (!s1)
-		return (ft_strdup(s2));
-	if (!s2)
-		return (ft_strdup(s1));
-	if (!(res = (char *)malloc(sizeof(char) * (ft_strlen(s1) +
-						ft_strlen(s2) + 1))))
-		return (NULL);
-	k = -1;
-	while (s1[++k])
-		res[k] = s1[k];
-	i = 0;
-	while (s2[i])
-		res[k++] = s2[i++];
-	res[k] = '\0';
-	return (res);
-}
-
-int				put_line(int fd, int i, char **str)
-{
-	int				res;
-	char			*temp;
-
-	res = -1;
-	temp = str[fd];
-	if (str[fd][i] != '\0')
+	fresh[len] = '\0';
+	while (len--)
 	{
-		str[fd] = ft_strdup(str[fd] + i + 1);
-		res = 1;
-	}
-	else
-	{
-		str[fd] = ft_strdup(str[fd] + i);
-		res = 0;
-	}
-	free(temp);
-	return (res);
-}
-
-int				get_line(int fd, char **line, char **str, int n)
-{
-	int					i;
-
-	i = 0;
-	if (str[fd] == NULL)
-		return (0);
-	while (str[fd][i] != '\0' && str[fd][i] != '\n')
+		fresh[i] = s[start];
 		i++;
-	if (str[fd][i] == '\n' || (str[fd][i] == '\0' && str[fd][0] != '\0'))
-	{
-		if (!(*line = ft_substr(str[fd], 0, i)))
-			return (-1);
-		return (put_line(fd, i, str));
+		start++;
 	}
-	if (!n && !*str[fd])
-		str[fd] = NULL;
-	return (0);
+	return (fresh);
 }
 
-int				get_next_line(int fd, char **line)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	static char				*str[LIMIT];
-	int						ret;
-	char					*tmp;
-	char					buffer[BUFFER_SIZE + 1];
+	char	*s3;
+	int		i;
+	int		j;
 
-	if (fd < 0 || fd > LIMIT || BUFFER_SIZE <= 0)
-		return (-1);
-	*str = ft_calloc(LIMIT, sizeof(char));
-	while (!(ft_strchr(buffer, '\n')) &&
-			(ret = read(fd, buffer, BUFFER_SIZE)) > 0)
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	if (!(s3 = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (NULL);
+	while (s1[i])
 	{
-		buffer[ret] = '\0';
-		tmp = str[fd];
-		if (!(str[fd] = ft_strjoin(str[fd], buffer)))
-			return (-1);
-		if (tmp)
-			free(tmp);
+		s3[i] = s1[i];
+		i++;
 	}
-	if (ret == -1)
-		return (-1);
-	return (get_line(fd, line, str, ret));
+	while (s2[j])
+	{
+		s3[i] = s2[j];
+		i++;
+		j++;
+	}
+	s3[i] = '\0';
+	return (s3);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	*a;
+
+	a = (unsigned char *)s;
+	while (*a)
+	{
+		if (*a == (unsigned char)c)
+			return ((char*)a);
+		a++;
+	}
+	if (*a == (unsigned char)c)
+		return ((char*)a);
+	return (NULL);
 }
